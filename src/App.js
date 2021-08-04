90f389a0c13681753ed4cf0b24f151c1d41ef752
sujatha-m <jhanavi.bhushan@gmail.com>
2021-08-04 15:47:07 -0400

    Implemented darkmode and light mode using matirial UI framework

--- src/App.js
+++ src/App.js
@@ -4,23 +4,55 @@ import './App.css'
 import Container from '@material-ui/core/Container'
 import Header from './components/Header/Header'
 import Definitions from './components/Definitions/Definitions'
+import { Switch } from '@material-ui/core'
+import { withStyles } from '@material-ui/core/styles'
+import { grey, indigo } from '@material-ui/core/colors'
 
-const styles = {
-  backgroundColor: '#282c34',
-  color: '#fff',
-  height: '100vh'
-}
+// const styles = {
+//   backgroundColor: LightMode ? '#fff' : '#282c34',
+//   color: LightMode ? 'black' : '#fff',
+//   height: '100vh'
+// }
 
-const styles1 = {
-  display: 'flex',
-  flexDirection: 'column',
-  height: '100vh'
-}
+// const styles1 = {
+//   display: 'flex',
+//   flexDirection: 'column',
+//   height: '100vh',
+//   justifyContent: 'space-evenly'
+// }
 
 function App () {
   const [word, setWord] = useState('')
   const [meanings, setMeanings] = useState([])
   const [category, setCategory] = useState('en')
+  const [LightMode, setLightMode] = useState(false)
+
+  const styles = {
+    backgroundColor: LightMode ? '#fff' : '#282c34',
+    color: LightMode ? 'black' : '#fff',
+    height: '100vh',
+    transition: 'all 0.5s linear'
+  }
+  const styles1 = {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100vh',
+    justifyContent: 'space-evenly'
+  }
+
+  const ThemeSwitch = withStyles({
+    switchBase: {
+      color: indigo[100],
+      '&$checked': {
+        color: indigo[600]
+      },
+      '&$checked + $track': {
+        backgroundColor: indigo[800]
+      }
+    },
+    checked: {},
+    track: {}
+  })(Switch)
 
   const dictionaryApi = async () => {
     try {
@@ -37,20 +69,35 @@ function App () {
 
   useEffect(() => {
     dictionaryApi()
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [word, category])
 
   return (
     <div className='App' style={styles}>
       <Container maxWidth='md' style={styles1}>
+        <div
+          style={{ position: 'absolute', top: 0, right: 15, paddingTop: 10 }}
+        >
+          <span>{LightMode ? 'Dark' : 'Light'} Mode</span>
+          <ThemeSwitch
+            checked={LightMode}
+            onChange={() => setLightMode(!LightMode)}
+          />
+        </div>
         <Header
           category={category}
           setCategory={setCategory}
           word={word}
           setWord={setWord}
+          LightMode={LightMode}
         />
         {meanings && (
-          <Definitions word={word} meanings={meanings} category={category} />
+          <Definitions
+            word={word}
+            meanings={meanings}
+            category={category}
+            LightMode={LightMode}
+          />
         )}
       </Container>
     </div>
--- src/components/Definitions/Definitions.js
+++ src/components/Definitions/Definitions.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import './Definitions.css'
 
-const Definitions = ({ word, meanings, category }) => {
+const Definitions = ({ word, meanings, category, LightMode }) => {
   return (
     <div className='meanings'>
+      {meanings[0] && word && category === 'en' && (
+        <audio
+          src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio}
+          style={{ backgroundColor: '#fff', borderRadius: '10px' }}
+          controls
+        >
+          Your Browser doesn't support audio element
+        </audio>
+      )}
       {word === '' ? (
         <span className='subTitle'>Start by typing a word in search</span>
       ) : (
@@ -12,7 +21,10 @@ const Definitions = ({ word, meanings, category }) => {
             item.definitions.map(def => (
               <div
                 className='singleMean'
-                style={{ backgroundColor: 'white', color: 'black' }}
+                style={{
+                  backgroundColor: LightMode ? '#3b5360' : 'white',
+                  color: LightMode ? 'white' : 'black'
+                }}
               >
                 <b>{def.definition}</b>
                 <hr style={{ backgroundColor: 'black', width: '100%' }}></hr>
